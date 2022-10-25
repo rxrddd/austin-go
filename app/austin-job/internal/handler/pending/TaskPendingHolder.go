61556d77a557aca01635ef0239e26c9da0ad73dd
zjy-mac <1812287739@qq.com>
2022-10-25 20:28:17 +0800

    fix:调整ants的size

--- app/austin-job/internal/handler/pending/TaskPendingHolder.go
+++ app/austin-job/internal/handler/pending/TaskPendingHolder.go
@@ -3,14 +3,14 @@ package pending
 import (
 	"austin-go/app/austin-common/taskUtil"
 	"context"
+	"fmt"
 	"github.com/panjf2000/ants/v2"
+	"runtime"
 )
 
 type TaskPendingHolder struct {
 	pool map[string]*ants.Pool
 }
-
-const defaultPoolLimit = 5000
 
 var defaultTaskPendingHolder = NewTaskPendingHolder()
 
@@ -18,9 +18,15 @@ func NewTaskPendingHolder() *TaskPendingHolder {
 	//初始化所有的链接池
 	groupIds := taskUtil.GetAllGroupIds()
 	pool := make(map[string]*ants.Pool)
+	size := runtime.NumCPU() * 2
 	for _, value := range groupIds {
-		newPool, _ := ants.NewPool(defaultPoolLimit)
-		pool[value] = newPool
+		var pushWorkerPool *ants.Pool
+		if wp, err := ants.NewPool(size); err != nil {
+			panic(fmt.Errorf("error occurred when creating push worker: %w", err))
+		} else {
+			pushWorkerPool = wp
+		}
+		pool[value] = pushWorkerPool
 	}
 	return &TaskPendingHolder{pool: pool}
 }
