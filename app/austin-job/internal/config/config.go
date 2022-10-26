8df8b4055cab8336f9a795092832dd96c2b134f2
zjy-mac <1812287739@qq.com>
2022-10-26 20:59:09 +0800

    fix:去掉job环境无用配置

--- app/austin-job/internal/config/config.go
+++ app/austin-job/internal/config/config.go
@@ -4,11 +4,9 @@ import (
 	"austin-go/common/dbx"
 	"austin-go/common/mq"
 	"github.com/zeromicro/go-zero/core/stores/cache"
-	"github.com/zeromicro/go-zero/rest"
 )
 
 type Config struct {
-	rest.RestConf
 	Rabbit     mq.RabbitConf
 	CacheRedis cache.CacheConf // redis缓存
 	Mysql      dbx.DbConfig
